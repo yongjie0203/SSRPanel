6bb7fff55804af89afe3623f916839194962d892
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-15 17:00:09 +0800

    提交测试

--- resources/views/marketing/addEmail.blade.php
+++ resources/views/marketing/addEmail.blade.php
@@ -44,14 +44,18 @@
                                     <div class="col-md-6">
                                         <label for="label" class="control-label col-md-1">用户标签</label>
                                         <div class="mt-checkbox-inline">
-                                            <label class="mt-checkbox">
-                                                <input type="checkbox" name="label" value="1" checked> 全体用户
-                                                <span></span>
-                                            </label>
-                                            <label class="mt-checkbox">
-                                                <input type="checkbox" name="label" value="2"> 仅有效用户
-                                                <span></span>
-                                            </label>
+                                            @if($labelList->isEmpty())                                   
+                                            @else
+                                                @foreach($labelList as $label)
+                                                    <label class="mt-checkbox">
+                                                        <input type="checkbox" name="label" value="{{$label->id}}" > {{$label->name}}
+                                                        <span></span>
+                                                    </label>
+                                                    
+                                                @endforeach
+                                            @endif
+                                         
+                                       
                                         </div>
                                     </div>
                                     
