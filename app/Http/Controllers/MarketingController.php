2d7e1cf3c8b286bd5e0b1544d9386158ae229b1f
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-17 17:28:11 +0800

    捕获异常处理

--- app/Http/Controllers/MarketingController.php
+++ app/Http/Controllers/MarketingController.php
@@ -190,27 +190,32 @@ class MarketingController extends Controller
         
     }
     
-    public function selectCount(Request $request){
-        $u = $request->get('u');
-        $t = $request->get('t');
-        //tr 多个之间的关系有可能为 or 、 and，如果不传默认为or
-        $tr = $request->get('tr');
-        $l = $request->get('l');
-        
-        $total = User:query()->count();
-        $query = DB::table('user')->selectRaw('count(DISTINCT user.username)');    
-           
-        if (!empty($t)) {
-            $query ->leftJoin('user_label', 'user.id', '=', 'user_label.user_id');
-            $query ->whereIn('user_label.id', split(",",$t));
+    public function emailReckon(Request $request){
+         try {
+            $u = $request->get('u');
+            $t = $request->get('t');
+            //tr 多个之间的关系有可能为 or 、 and，如果不传默认为or
+            $tr = $request->get('tr');
+            $l = $request->get('l');
+
+            $total = User:query()->count();
+            $query = DB::table('user')->selectRaw('count(DISTINCT user.username)');    
+
+            if (!empty($t)) {
+                $query ->leftJoin('user_label', 'user.id', '=', 'user_label.user_id');
+                $query ->whereIn('user_label.id', split(",",$t));
+            }
+            if(!empty($l)){        
+                $query->whereIn('user.level', split(",",$l));
+            }
+            if(!empty($u)){        
+                $query->whereIn('user.status', split(",",$u));
+            }
+            $selected = $query-> get();
+            return Response::json(['status' => 'success', 'data' => ['total'=>$total,'selected'=>$selected], 'message' => '成功']);
+        } catch (\Exception $e) {
+           return $e->getMessage();
         }
-        if(!empty($l)){        
-            $query->whereIn('user.level', split(",",$l));
-        }
-        if(!empty($u)){        
-            $query->whereIn('user.status', split(",",$u));
-        }
-        $selected = $query-> get();
-        return Response::json(['status' => 'success', 'data' => ['total'=>$total,'selected'=>$selected], 'message' => '成功']);
     }
+    
 }
