6442b39bd780b170a6f0b54b0de5f6e1b8f5233b
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-20 16:19:56 +0800

    节点最近30天的使用情况

--- app/Http/Controllers/DataCenterController.php
+++ app/Http/Controllers/DataCenterController.php
@@ -35,5 +35,16 @@ class DataCenterController extends Controller
         return Response::view('admin.dataCenter');
    }
     
+   public function nodeUsedMonthly(){
+       $dbdata = DB::table('ss_node sn')
+                    ->selectRaw('sn.id,sn.`name`,if(sum(l.u+l.d) is null ,0,sum(l.u+l.d))/(1024*1024*1024) used')
+                    ->leftJoin('user_traffic_log l','l.node_id','=','sn.id')
+                    ->groupBy('l.node_id')
+                    ->groupBy('sn.name')
+                    ->orderBy('sum(l.u+l.d)','desc')
+                    ->get();
+       return Response::json(['status' => 'success', 'data' => $dbdata, 'message' => '成功']);
+   }
+    
     
 }
