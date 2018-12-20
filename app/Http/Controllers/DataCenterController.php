15ee09f517ac9c9df9220dfad7d4ba7d982c5626
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-20 11:42:42 +0800

    添加数据中心控制层

---
+++ app/Http/Controllers/DataCenterController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Components\Helpers;
+use App\Http\Models\Email;
+use App\Http\Models\Label;
+use App\Http\Models\Level;
+use App\Http\Models\User;
+use App\Mail\freeMail;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Response;
+use Log;
+use DB;
+use Auth;
+use Mail;
+
+/**
+ * 数据中心控制器
+ *
+ * Class DataCenterController
+ *
+ * @package App\Http\Controllers
+ */
+class DataCenterController extends Controller
+{
+    protected static $systemConfig;
+
+    function __construct()
+    {
+        self::$systemConfig = Helpers::systemConfig();
+    }
+
+   
+    
+    
+}
