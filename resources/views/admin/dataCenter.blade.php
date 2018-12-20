a47070b3e9471f9e5a9714cbe7cd791e4fd68057
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-20 19:49:30 +0800

    提交测试

--- resources/views/admin/dataCenter.blade.php
+++ resources/views/admin/dataCenter.blade.php
@@ -6,8 +6,8 @@
     <div class="page-content" style="padding-top:0;">
         <!-- BEGIN PAGE BASE CONTENT -->
         <div >
-            ceshi
-            <div id="node-used-monthly" style="width: 600px;height:400px;"></div>
+           
+            <div id="node-used-monthly" style="width:800px;height=100px;" ></div>
         </div> 
         
         
@@ -20,47 +20,62 @@
     <script src="/assets/global/plugins/echarts/echarts.min.js" type="text/javascript"></script>
     
     <script type="text/javascript">
-        var nodeUsedMonthlyEchart = echarts.init(document.getElementById('node-used-monthly'));
-        // 指定图表的配置项和数据
-        var option = {
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        };
-
-        // 使用刚指定的配置项和数据显示图表。
-        nodeUsedMonthlyEchart.setOption(option);
-        
          $(function() {
+             var nodeUsedMonthlyEchart = echarts.init(document.getElementById('node-used-monthly'));
+             nodeUsedMonthly();
              
-             
-            /* function nodeUsedMonthly(){
+             function nodeUsedMonthly(){
                  $.ajax({
                     type: "GET",
                     url: "{{url('dataCenter/nodeUsedMonthly')}}",
                     async: false,                  
                     success: function (ret) {                        
-                        if (ret.status == 'success') {     
-
+                        if (ret.status == 'success') {                          
+                         var option = {
+                                title: {
+                                    text: '节点最近30天内使用量'
+                                },
+                                color: ['#3398DB'],
+                                tooltip : {
+                                    trigger: 'axis',
+                                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                                    }
+                                },
+                                grid: {
+                                    left: '3%',
+                                    right: '4%',
+                                    bottom: '3%',
+                                    containLabel: true
+                                },
+                                xAxis : [
+                                    {
+                                        type : 'category',
+                                        data : ret.data.x,
+                                        axisTick: {
+                                            alignWithLabel: true
+                                        }
+                                    }
+                                ],
+                                yAxis : [
+                                    {
+                                        type : 'value'
+                                    }
+                                ],
+                                series : [
+                                    {
+                                        name:'用量',
+                                        type:'bar',
+                                        barWidth: '60%',
+                                        data:ret.data.y
+                                    }
+                                ]
+                            };
                             nodeUsedMonthlyEchart.setOption(option);
                         }                       
                     }
                 });
-             }*/
-             
+             }
          });
     </script>
 @endsection
