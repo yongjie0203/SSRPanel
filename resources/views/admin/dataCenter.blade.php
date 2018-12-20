79ba1613e5b153be604e7cb5e1ab9b0f498c913d
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-20 16:36:06 +0800

    提交测试

--- resources/views/admin/dataCenter.blade.php
+++ resources/views/admin/dataCenter.blade.php
@@ -6,10 +6,11 @@
     <div class="page-content" style="padding-top:0;">
         <!-- BEGIN PAGE BASE CONTENT -->
         <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-               
-            </div>
-        </div>  
+            <div id="node-used-monthly" class="col-lg-3 col-md-3 col-sm-6 col-xs-12"></div>
+        </div> 
+        <div class="row">
+            <div id="node-hot-monthly" class="col-lg-3 col-md-3 col-sm-6 col-xs-12"></div>
+        </div>
         
      
         <!-- END PAGE BASE CONTENT -->
@@ -21,7 +22,59 @@
     
     <script type="text/javascript">
          $(function() {
-         
+             var nodeUsedMonthlyEchart = echarts.init(document.getElementById('node-used-monthly'));
+             nodeUsedMonthly();
+             
+             function nodeUsedMonthly(){
+                 $.ajax({
+                    type: "GET",
+                    url: "{{url('dataCenter/nodeUsedMonthly')}}",
+                    async: false,                  
+                    success: function (ret) {                        
+                        if (ret.status == 'success') {                         
+                            app.title = '节点最近30天内使用量';
+                            option = {
+                                color: ['#3398DB'],
+                                tooltip : {
+                                    trigger: 'axis',
+                                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                                    }
+                                },
+                                grid: {
+                                    left: '3%',
+                                    right: '4%',
+                                    bottom: '3%',
+                                    containLabel: true
+                                },
+                                xAxis : [
+                                    {
+                                        type : 'category',
+                                        data : ret.data.x,
+                                        axisTick: {
+                                            alignWithLabel: true
+                                        }
+                                    }
+                                ],
+                                yAxis : [
+                                    {
+                                        type : 'value'
+                                    }
+                                ],
+                                series : [
+                                    {
+                                        name:'直接访问',
+                                        type:'bar',
+                                        barWidth: '60%',
+                                        data:ret.data.y
+                                    }
+                                ]
+                            };
+                            nodeUsedMonthlyEchart.setOption(option);
+                        }                       
+                    }
+                });
+             }
          });
     </script>
 @endsection
