ba1282373b030a3004e06530af043cab2203cbd2
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-22 13:56:38 +0800

    提交测试

--- resources/views/marketing/addEmail.blade.php
+++ resources/views/marketing/addEmail.blade.php
@@ -191,7 +191,7 @@
                       type: 1,
                       skin: 'layui-layer-rim', //加上边框
                       area: ['420px', '240px'], //宽高
-                      content: $("userGroup")
+                      content: $("#userGroup")
                     });
              });
              
