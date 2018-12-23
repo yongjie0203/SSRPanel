9d8b605b6153a6448ae2c67bb59088fe1d389e2c
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-23 20:31:04 +0800

    提交测试

--- app/Http/Controllers/DataCenterController.php
+++ app/Http/Controllers/DataCenterController.php
@@ -53,8 +53,8 @@ class DataCenterController extends Controller
    public function userOnlineDataMonthly(){
         $dbdata = DB::table('user_traffic_log')
                     ->selectRaw("FROM_UNIXTIME( user_traffic_log.log_time,'%H') hours, count(distinct user_traffic_log.user_id) users, count(user_traffic_log.id ) time")
-                    ->groupBy("FROM_UNIXTIME( user_traffic_log.log_time,'%H')")
-                    ->orderBy("FROM_UNIXTIME( user_traffic_log.log_time,'%H')","desc")
+                    ->groupBy("hours,'%H')")
+                    ->orderBy("hours","desc")
                     ->get()
                     ->toArray();
        $hours = array_column($dbdata,'hours');
