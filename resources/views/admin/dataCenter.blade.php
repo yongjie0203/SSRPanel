71b68585a56b3a3ab232d3db5fdbb1f0a226724c
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-23 20:26:43 +0800

    提交测试

--- resources/views/admin/dataCenter.blade.php
+++ resources/views/admin/dataCenter.blade.php
@@ -24,7 +24,7 @@
              var nodeUsedMonthlyEchart = echarts.init(document.getElementById('node-used-monthly'));
              var userOnlineDataMonthlyEchart = echarts.init(document.getElementById('user-online-data-monthly'));
              nodeUsedMonthly();
-             var option = {title:{text:'节点近30天内使用量'},color:['#3398DB'],tooltip:{trigger:'axis',axisPointer:{type:'shadow'}},grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},xAxis:[{type:'category',axisLabel:{interval:0,rotate:-40},axisTick:{alignWithLabel:true}}],yAxis:[{type:'value'}],series:[{name:'用量',type:'bar',barWidth:'60%'}]};
+             
              
              function nodeUsedMonthly(){
                  $.ajax({
@@ -33,23 +33,8 @@
                     async: false,                  
                     success: function (ret) {                        
                         if (ret.status == 'success') {  
-                            nodeUsedMonthlyEchart.setOption(option);
-                            nodeUsedMonthlyEchart.setOption({
-                                 title: {text: '节点近30天内使用量'},
-                                 xAxis: [{type: 'category',
-                                          data: ret.data.x,
-                                          axisLabel: {interval: 0,rotate: -40 },
-                                          axisTick: {alignWithLabel: true}
-                                        }],
-                                 yAxis: [ {type: 'value'}],
-                                      series: [{
-                                          name: '用量',
-                                          type: 'bar',
-                                          barWidth: '60%',
-                                          data: ret.data.y
-                                        }
-                                      ]
-                            });
+                            var option = {title:{text:'节点近30天内使用量'},color:['#3398DB'],tooltip:{trigger:'axis',axisPointer:{type:'shadow'}},grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},xAxis:[{type:'category',data:ret.data.x,axisTick:{alignWithLabel:true},axisLabel:{interval:0,rotate:-40},axisTick:{alignWithLabel:true}}],yAxis:[{type:'value'}],series:[{name:'用量',type:'bar',barWidth:'60%',data:ret.data.y}]};
+                            nodeUsedMonthlyEchart.setOption(option);                           
                         }                       
                     }
                 });
@@ -62,22 +47,8 @@
                     async: false,                  
                     success: function (ret) {                        
                         if (ret.status == 'success') {  
-                             nodeUsedMonthlyEchart.setOption(option);
-                             nodeUsedMonthlyEchart.setOption({
-                                 title: {text: '近30天用户上网时间分布'},
-                                 xAxis: [{type: 'category',
-                                          data: ret.data.hours,                                         
-                                          axisTick: {alignWithLabel: true}
-                                        }],
-                                 yAxis: [ {type: 'value'}],
-                                      series: [{
-                                          name: '在线用户数',
-                                          type: 'line',
-                                          barWidth: '60%',
-                                          data: ret.data.users
-                                        }
-                                      ]
-                            });
+                            var option = {title:{text:'近30天用户在线时间分布'},xAxis:{type:'category',data:ret.data.hours},yAxis:{type:'value'},series:[{data:ret.data.user,type:'line'}]};;
+                            userOnlineDataMonthlyEchart.setOption(option);                            
                         }                       
                     }
                 });
