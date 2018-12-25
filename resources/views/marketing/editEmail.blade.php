7019dffa6c8301db1c443dec128eb7b24e1da32f
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-25 11:24:31 +0800

    提交测试

--- resources/views/marketing/editEmail.blade.php
+++ resources/views/marketing/editEmail.blade.php
@@ -17,66 +17,94 @@
                 <div class="portlet light bordered">
                     <div class="portlet-title">
                         <div class="caption">
-                            <span class="caption-subject font-darm sbold uppercase">编辑文章</span>
+                            <span class="caption-subject font-dark sbold uppercase">邮件群发</span>
                         </div>
                         <div class="actions"></div>
                     </div>
                     <div class="portlet-body form">
                         <!-- BEGIN FORM-->
-                        <form action="{{url('admin/editArticle')}}" method="post" enctype="multipart/form-data" class="form-horizontal" onsubmit="return do_submit();">
+                        <form action="{{url('marketing/editEmail')}}" method="post" enctype="multipart/form-data" class="form-horizontal" onsubmit="return do_submit();">
                             <div class="form-body">
                                 <div class="form-group">
-                                    <label for="type" class="control-label col-md-1">类型</label>
-                                    <div class="col-md-6">
-                                        <div class="mt-radio-inline">
-                                            <label class="mt-radio">
-                                                <input type="radio" name="type" value="1" {{$article->type == '1' ? 'checked' : ''}}> 文章
-                                                <span></span>
-                                            </label>
-                                            <label class="mt-radio">
-                                                <input type="radio" name="type" value="2" {{$article->type == '2' ? 'checked' : ''}}> 公告
-                                                <span></span>
-                                            </label>
+                                    <label class="control-label col-md-1">选择范围</label>
+                                    <div class="col-md-11" >
+                                        <div class="mt-checkbox-inline">
+                                            <button type="button" id="selectUser" class="btn green">选择</button>
                                         </div>
+                                         <label id="count_info" style="color:red;"></label>
+                                    </div>
+
+                               <div class="form-group">
+                                    <label class="control-label col-md-1">使用模板</label>
+                                    <div class="col-md-11">    
+                                        <label class="mt-checkbox">
+                                             <input type="checkbox" name="template" checked value="1" ></input>
+                                             <span></span>
+                                        </label>
                                     </div>
                                 </div>
+                                
                                 <div class="form-group">
-                                    <label class="control-label col-md-1">标题</label>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" name="title" value="{{$article->title}}" id="title" placeholder="" autofocus required>
+                                    <label class="control-label col-md-1">邮件格式</label>
+                                    <div class="col-md-11">
+                                        <label class="mt-radio">
+                                             <input type="radio" name="format" value="1" > html </input>
+                                             <span></span>
+                                        </label>
+                                        <label class="mt-radio">
+                                             <input type="radio" name="format" value="2" > markdown </input>
+                                             <span></span>
+                                         </label> 
+                                    </div>
+                                </div>
+
+                                
+                                <div class="form-group">
+                                    <label class="control-label col-md-1">发送模式</label>
+                                    <div class="col-md-11">
+                                        <label class="mt-radio">
+                                             <input type="radio" name="mode" value="1" > 单封单人 </input>
+                                             <span></span>
+                                        </label>
+                                        <label class="mt-radio">
+                                             <input type="radio" name="mode" value="2" > 单封多人 </input>
+                                             <span></span>
+                                         </label> 
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="control-label col-md-1">收件人</label>
+                                    <div class="col-md-10">
+                                        <input type="text" class="form-control" name="to" id="to" placeholder="">
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label class="control-label col-md-1">主题</label>
+                                    <div class="col-md-5">
+                                        <input type="text" class="form-control" name="subject" id="subject" placeholder="" autofocus required>
                                         <input type="hidden" name="_token" value="{{csrf_token()}}">
                                     </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-1">简介</label>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" name="summary" value="{{$article->summary}}" id="summary" placeholder="">
+                                    <label class="control-label col-md-1">标题</label>
+                                    <div class="col-md-4">
+                                       <input type="text" class="form-control" name="title" id="title" placeholder=""/>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="control-label col-md-1">作者</label>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" name="author" value="{{$article->author}}" id="author" placeholder="" required>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-1">排序</label>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" name="sort" value="{{$article->sort}}" id="sort" required />
-                                        <span class="help-block"> 值越高显示时越靠前 </span>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-1">内容</label>
+                                    <label class="control-label col-md-1">邮件内容</label>
                                     <div class="col-md-10">
-                                        <script id="editor" type="text/plain" style="height:400px;">{!! $article->content !!}</script>
+                                        <script id="editor" type="text/plain" style="height:400px;"></script>
                                     </div>
                                 </div>
                             </div>
                             <div class="form-actions">
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <button type="submit" class="btn green">提 交</button>
+                                        <input type="hidden" id="action" name="action"  value="save" ></input>
+                                        <button type="submit" onclick="$('#action').val($(this).attr('name'))" name="save" class="btn green">保存当前内容</button>
+                                        <button type="submit" onclick="$('#action').val($(this).attr('name'))" name="test" class="btn green">在邮箱中预览</button>
+                                        <button type="submit" onclick="$('#action').val($(this).attr('name'))" name="start" class="btn green">启动群发任务</button>
                                     </div>
                                 </div>
                             </div>
@@ -89,6 +117,40 @@
         </div>
         <!-- END PAGE BASE CONTENT -->
     </div>
+    
+   <div class="col-md-6 col-xs-11" >
+        <div id="userGroup"  style="display:none;">
+            <div class="form-group">
+                <div class="col-md-12">                
+                    <div class="mt-checkbox-inline" >
+                         @if(!$groupList->isEmpty())
+                            @foreach($groupList as $group)
+                                <label class="mt-checkbox">
+                                <input type="checkbox" name="group" value="{{$group->id}}" > {{$group->name}}
+                                <span></span>
+                            </label>                                                   
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div id="selectedInfo" class="col-md-12" style="color:red;line-height: 30px">
+                
+                </div>
+            </div>
+
+
+            <div class="form-group">
+                <div class="col-md-12" style="text-align: right;padding-right: 30px;">                    
+                    <button type="button" id="closeSelect" class="btn green">确定</button>
+                </div>
+            </div>
+
+
+        </div>
+     </div>
     <!-- END CONTENT BODY -->
 @endsection
 @section('script')
@@ -101,7 +163,7 @@
             toolbars:[['source','undo','redo','bold','italic','underline','insertimage','insertvideo','lineheight','fontfamily','fontsize','justifyleft','justifycenter','justifyright','justifyjustify','forecolor','backcolor','link','unlink']],
             wordCount:true,                //关闭字数统计
             elementPathEnabled : false,    //是否启用元素路径
-            maximumWords:300,              //允许的最大字符数
+            maximumWords:1000,              //允许的最大字符数
             initialContent:'',             //初始化编辑器的内容
             initialFrameWidth:null,        //初始化宽度
             autoClearinitialContent:false, //是否自动清除编辑器初始内容
@@ -110,24 +172,30 @@
         // ajax同步提交
         function do_submit() {
             var _token = '{{csrf_token()}}';
-            var id = '{{$article->id}}';
             var title = $('#title').val();
-            var type = $("input:radio[name='type']:checked").val();
-            var author = $('#author').val();
-            var summary = $('#summary').val();
-            var sort = $('#sort').val();
+            var template = $("input[name='template']").is(':checked') ? 1 : 0;
+            var format = $("input[name='format']:checked").val();
+            var mode = $("input[name='mode']:checked").val();
+            var to = $('#to').val();
+            var subject = $('#subject').val(); 
+            var action = $('#action').val();
             var content = UE.getEditor('editor').getContent();
+            if("2" == format){
+                content = UE.getEditor('editor').getPlainTxt();
+            }
+            
+            var groups = getSelectedGroup().join(",");
 
             $.ajax({
                 type: "POST",
-                url: "{{url('admin/editArticle')}}",
+                url: "{{url('marketing/editEmail')}}",
                 async: false,
-                data: {_token:_token, id:id, title: title, type:type, author:author, summary:summary, sort:sort, content:content},
+                data: {_token:_token, title: title, groups:groups,template:template, format:format, mode:mode, content:content, to:to ,subject:subject,action:action},
                 dataType: 'json',
                 success: function (ret) {
                     layer.msg(ret.message, {time:1000}, function() {
                         if (ret.status == 'success') {
-                            window.location.href = '{{url('admin/articleList')}}';
+                            window.location.href = '{{url('marketing/emailList')}}';
                         }
                     });
                 }
@@ -135,5 +203,63 @@
 
             return false;
         }
+        
+        function getSelectedGroup(){
+            var groups = new Array();
+            $("input[name='group']:checked").each(function(){
+                groups.push($(this).val())
+            });
+            return groups;
+        }
+
+        
+        $(function() {
+             var index;
+             $(document).on('click','#selectUser',function(){
+                var width = $('#userGroup').outerWidth(true);
+                var height = $('#userGroup').outerHeight(true);
+                height = height + 50;
+                height = height +'px';
+                width = width + 50;
+                width = width +'px'
+                index = layer.open({
+                      type: 1,     
+                      title:'选择群发分组',
+                      area: [width,height],
+                      content: $("#userGroup")
+                    });
+             });
+             
+             $(document).on('click','#closeSelect',function(){
+                layer.close(index);
+                $('#count_info').text($("#selectedInfo").text());
+             });
+             
+             $(document).on('click', "input[name='group']", function(){
+                var groups = getSelectedGroup().join(",");
+               
+                console.log(groups);
+             
+                $.ajax({
+                    type: "GET",
+                    url: "{{url('marketing/getGroupCount')}}",
+                    async: false,
+                    data: {groups:groups},
+                    dataType: 'json',
+                    success: function (ret) {                        
+                        if (ret.status == 'success') {
+                            var count = "总用户数："+ret.data.total+"，已选择：" + ret.data.selected[0].selected+"，退订："+ ret.data.selected[0].blacked+"，转发：" +ret.data.selected[0].forward ;
+                            $("#selectedInfo").text(count);
+                        }                       
+                    }
+                });
+               
+            });
+            
+                        
+                      
+        });
+        
+        
     </script>
 @endsection
