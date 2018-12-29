27f350d2b8f5c8d30ede12b1c2d58e0d486bf2bb
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-29 13:55:21 +0800

    提交测试

--- app/Listeners/LogSentMessage.php
+++ app/Listeners/LogSentMessage.php
@@ -6,6 +6,7 @@ use Illuminate\Mail\Events\MessageSent;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Http\Models\EmailTask;
+use App\Http\Models\Email;
 use Log;
 
 
@@ -32,7 +33,10 @@ class LogSentMessage
         $task = $event->data->task;
         if(!empty($task)){
             $data = ['status'=>1];//已发送
-            EmailTask::query()->where('id', $task->id)->update($data);            
+            EmailTask::query()->where('id', $task->id)->update($data);
+            $eamil = Email::query()->where('id', $task->email_id)->first();
+            $emailData = ['send'=> $eamil->send + 1];//发送数量加1
+            Email::query()->where('id', $task->email_id)->update($emailData);
         }
        
     }
