3ca5b16c305658bad836889b5bd812ab8a463b4e
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-01-03 16:06:41 +0800

    提交测试

--- app/Http/Controllers/DataCenterController.php
+++ app/Http/Controllers/DataCenterController.php
@@ -64,29 +64,7 @@ class DataCenterController extends Controller
    }
     
    public function orderDataLast30Day(){
-       /* $sql = "SELECT t.date,if(o.amount is null ,0,o.amount) amount from ( ";
-        $sql .= "SELECT DATE_FORMAT(date_add(now(), interval -1*x.d day),'%Y-%m-%d') date ";
-        $sql .= " FROM ";
-        $sql .= " (SELECT 0 AS d UNION ALL  SELECT 1   UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL ";
-        $sql .= " SELECT 8 UNION ALL SELECT 9 UNION ALL SELECT 10 UNION ALL SELECT 11 UNION ALL SELECT 12 UNION ALL SELECT 13 UNION ALL SELECT 14 UNION ALL ";
-        $sql .= " SELECT 15 UNION ALL SELECT 16 UNION ALL SELECT 17 UNION ALL SELECT 18 UNION ALL SELECT 19 UNION ALL SELECT 20 UNION ALL SELECT 21 UNION ALL ";
-        $sql .= " SELECT 22 UNION ALL SELECT 23 UNION ALL SELECT 24 UNION ALL SELECT 25 UNION ALL SELECT 26 UNION ALL SELECT 27 UNION ALL SELECT 28 UNION ALL ";
-        $sql .= " SELECT 29 UNION ALL SELECT 30 ) x) ";
-        $sql .= "     t  ";
-        $sql .= "     LEFT JOIN ( ";
-        $sql .= "     SELECT DATE_FORMAT(`order`.created_at,'%Y-%m-%d') date ,sum(if(amount = 0 ,origin_amount,amount))/100 amount from `order`  ";
-        $sql .= "     where `order`.`status` in (1,2) ";
-        $sql .= "     GROUP BY DATE_FORMAT(`order`.created_at,'%Y-%m-%d') ";
-        $sql .= "     ) o on t.date = o.date ";
-        $sql .= "     order by t.date";
-        return $sql;
-        $dbdata = DB::table(DB:raw('($sql) as t'))
-                    ->selectRaw('date,amount')
-                    ->get()
-                    ->toArray();
-        $date = array_column($dbdata,'date');
-        $amount = array_column($dbdata,'amount');
-        return Response::json(['status' => 'success', 'data' => ['date'=>$date,'amount'=>$amount], 'message' => '成功']);*/
+       
        return "ok";
    }
 }
