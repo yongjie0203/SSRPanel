d67fabf9dd5aba4a1640b0854d60c8f8b84801c5
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-01-06 17:53:35 +0800

    提交测试

--- resources/views/admin/dataCenter.blade.php
+++ resources/views/admin/dataCenter.blade.php
@@ -42,7 +42,7 @@
                     async: false,                  
                     success: function (ret) {                        
                         if (ret.status == 'success') {  
-                            var option = {title:{text:'重置日至今使用量'},color:['#3398DB'],tooltip:{trigger:'axis',axisPointer:{type:'shadow'}},grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},xAxis:[{type:'category',data:ret.data.name,axisTick:{alignWithLabel:true},axisLabel:{interval:0,rotate:-40},axisTick:{alignWithLabel:true}}],yAxis:[{type:'value'}],series:[{name:'用量',type:'bar',barWidth:'60%',data:ret.data.percent}]};
+                            var option = {title:{text:'重置日至今使用量'},color:['#3398DB'],tooltip:{trigger: 'item',formatter: '{b}:\n{c}%',axisPointer:{type:'shadow'}},grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},xAxis:[{type:'category',data:ret.data.name,axisTick:{alignWithLabel:true},axisLabel:{interval:0,rotate:-40},axisTick:{alignWithLabel:true}}],yAxis:[{type:'value',axisLabel: {show: true,interval: 'auto',formatter: '{value} %' }}],series:[{name:'用量',type:'bar',barWidth:'60%',data:ret.data.percent}]};
                             nodeUsedCyclicityEchart.setOption(option);                           
                         }                       
                     }
