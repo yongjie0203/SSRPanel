c7b223098fb37374b99e0fe307a7111ccc18024c
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-01-06 16:26:22 +0800

    添加节点流量重置周期内使用量统计

--- resources/views/admin/dataCenter.blade.php
+++ resources/views/admin/dataCenter.blade.php
@@ -6,6 +6,7 @@
     <div class="page-content" style="padding-top:0;">
         <!-- BEGIN PAGE BASE CONTENT -->
         <div >          
+            <div id="node-used-cyclicity" style="width:95%;height:300px" ></div>
             <div id="node-used-monthly" style="width:95%;height:300px" ></div>
             <div id="user-online-data-monthly" style="width:95%;height:300px" ></div>
             <div id="order-data-monthly" style="width:95%;height:300px" ></div>
@@ -22,12 +23,31 @@
     
     <script type="text/javascript">
          $(function() {
+         
+             var nodeUsedCyclicityEchart = echarts.init(document.getElementById('node-used-cyclicity'));
              var nodeUsedMonthlyEchart = echarts.init(document.getElementById('node-used-monthly'));
              var userOnlineDataMonthlyEchart = echarts.init(document.getElementById('user-online-data-monthly'));
              var orderDataMonthlyEchart = echarts.init(document.getElementById('order-data-monthly'));
+             
+             nodeUsedCyclicity();
              nodeUsedMonthly();
              userOnlinDataMonthly();
              orderDataMonthly();
+             
+             //流量重置日至今使用量百分百
+             function nodeUsedCyclicity(){
+                 $.ajax({
+                    type: "GET",
+                    url: "{{url('dataCenter/nodeUsedCyclicity')}}",
+                    async: false,                  
+                    success: function (ret) {                        
+                        if (ret.status == 'success') {  
+                            var option = {title:{text:'重置日至今使用量'},color:['#3398DB'],tooltip:{trigger:'axis',axisPointer:{type:'shadow'}},grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},xAxis:[{type:'category',data:ret.data.name,axisTick:{alignWithLabel:true},axisLabel:{interval:0,rotate:-40},axisTick:{alignWithLabel:true}}],yAxis:[{type:'value'}],series:[{name:'用量',type:'bar',barWidth:'60%',data:ret.data.percent}]};
+                            nodeUsedCyclicityEchart.setOption(option);                           
+                        }                       
+                    }
+                });
+             }
              
              //节点30天用量
              function nodeUsedMonthly(){
