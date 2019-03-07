32376ad78ca89d0bb07feb56939782da7244c272
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-03-07 15:28:44 +0800

    用户总流量显示不限流量

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -296,7 +296,12 @@
                                 </div>
                                 <div class="form-group" style="margin-bottom: 0;">
                                     <label class="col-md-4 control-label">{{trans('home.account_bandwidth_usage')}}：</label>
+                                    @if($info['totalTransfer'] <= 5*1024*1024*1024)
                                     <p class="form-control-static"> {{$info['usedTransfer']}}（{{$info['totalTransfer']}}） </p>
+                                    @endif
+                                    @if($info['totalTransfer'] > 5*1024*1024*1024)
+                                    <p class="form-control-static"> {{$info['usedTransfer']}}（不限流量） </p>
+                                    @endif
                                 </div>
                                 @if($info['traffic_reset_day'])
                                     <div class="form-group" style="margin-bottom: 0;">
