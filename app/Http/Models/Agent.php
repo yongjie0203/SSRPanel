184106e1bcd079249461f89d9c8d134bd671accd
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-04-29 21:11:31 +0800

    创建代理类

---
+++ app/Http/Models/Agent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 代理信息
+ * Class Agent
+ *
+ * @package App\Http\Models
+ * @mixin \Eloquent
+ */
+class Agent extends Model
+{
+    protected $table = 'agent';
+    protected $primaryKey = 'id';
+
+}
