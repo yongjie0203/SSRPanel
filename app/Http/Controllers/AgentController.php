33baff691f90c88c5b572e79867f1491225b50c1
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-05-02 12:01:52 +0800

    日常提交

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -60,6 +60,7 @@ class AgentController extends Controller
        $order_by = $status == 0 ? 'available_end' : 'updated_at';
        $soft = $status == 0 ? 'asc' : 'desc';
        $p = $status .' '. $limit .' '. $amount .' '. $order_by .' '. $soft;
+       Log::error('测试信息：' . $p);
        //$couponList = Coupon::query()->where('holder',Auth::user()->id)->where('status',$status)->where('amount',$amount)->limit($limit)->get()->toArray();
        Response::json(['status' => 'success', 'data' => $p , 'message' => '']);
    }
