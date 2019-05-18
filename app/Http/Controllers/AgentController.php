a2ef5c3a7abcd77f75e698bbd758974eaeba38b7
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-05-19 02:24:15 +0800

    修正代码

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -200,7 +200,7 @@ class AgentController extends Controller
                 Order::query()->where('coupon_id', $coupon->id)->update(['status' => 3]);
                 // 用户代理禁用，可用流量置零
                 User::query()->where('id', $order->user_id)->update(['expire_time' => date("Y-m-d H:i:s", strtotime("+" . "1" . " days")) ,'u' => 0, 'd' => 0, 'transfer_enable' => 0,'enable' => 0]);
-                Helpers::addUserTrafficModifyLog($user->id, $order->oid, 0, 0, '[退款]用户退款，可用流量置零');
+                Helpers::addUserTrafficModifyLog($order->user_id, $order->oid, 0, 0, '[退款]用户退款，可用流量置零');
                 return Response::json(['status' => 'success', 'data' => '' , 'message' => '用户订单已取消，可用流量清零']);
             }
             return Response::json(['status' => 'fail', 'data' => '' , 'message' => '该码暂未使用，已禁止使用']);
